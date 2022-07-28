528c0b1572624d869994603d91d3e0d32038a812
Hongjin Li <vic.hong@rock-chips.com>
2022-07-28 10:07:42 +0800

    更新自动备份脚本

--- lhjbackup.sh
+++ lhjbackup.sh
@@ -9,20 +9,18 @@
 # crontab -e
 # * 4 * * * sh ~/Projects/Some-program/lhjbackup.sh
 
-usrHome="/Users/$(whoami)"
-
 # src
-srcDirPerLoc="$usrHome/Documents/doc"
-srcDirPerNetRoot="$usrHome/Documents"
+srcDirPerLoc="$HOME/Documents/doc"
+srcDirPerNetRoot="$HOME/Documents"
 srcDirPerNet="doc"
 
-srcDirWorkLoc="$usrHome/Documents/rk-doc"
-srcDirWorkNetRoot="$usrHome/Documents/rk-doc"
+srcDirWorkLoc="$HOME/Documents/rk-doc"
+srcDirWorkNetRoot="$HOME/Documents/rk-doc"
 srcDirWorkNet="2.工作资料 3.输出资料 4.总结汇报"
 
 # dst
 dstDirLoc="/Volumes/exdisk/document"
-dstDirNet="$usrHome/Library/CloudStorage/OneDrive-Personal/文档"
+dstDirNet="$HOME/Library/CloudStorage/OneDrive-Personal/文档"
 
 echo "==> backup per dir to loc"
 time cp -r $srcDirPerLoc $dstDirLoc 2> /dev/null
@@ -34,14 +32,17 @@ time cp -r $srcDirWorkLoc $dstDirLoc 2> /dev/null
 echo
 
 echo "==> backup per dir to net"
-time tar -zcPvf $dstDirNet/perbackup$(date +%Y%m%d).tar.gz -C $srcDirPerNetRoot $srcDirPerNet 2> /dev/null
+time tar -zcPvf $dstDirLoc/netBackupTmp/perbackup$(date +%Y%m%d).tar.gz -C $srcDirPerNetRoot $srcDirPerNet 2> /dev/null
+time cp $dstDirLoc/netBackupTmp/perbackup$(date +%Y%m%d).tar.gz $dstDirNet
 echo
 echo "==> backup work dir to net"
-time tar -zcPvf $dstDirNet/workbackup$(date +%Y%m%d).tar.gz -C $srcDirWorkNetRoot \
+time tar -zcPvf $dstDirLoc/netBackupTmp/workbackup$(date +%Y%m%d).tar.gz -C $srcDirWorkNetRoot \
     --exclude="./2.工作资料/kernel编译/prebuilts" --exclude="./2.工作资料/kernel编译/prebuilts.zip" \
     --exclude="./2.工作资料/协议" \
     $srcDirWorkNet 2> /dev/null
+time cp $dstDirLoc/netBackupTmp/workbackup$(date +%Y%m%d).tar.gz $dstDirNet
 echo
 
 #删除文件夹下超过30天的文件
+find $dstDirLoc/netBackupTmp -mtime +30 -name "*.tar.gz" -exec rm -rf {} \;
 find $dstDirNet -mtime +30 -name "*.tar.gz" -exec rm -rf {} \;
